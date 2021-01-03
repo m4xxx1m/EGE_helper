1564907d370b354253693e6e52781fdb4476223d
m4xxx1m <mi4469228@yandex.ru>
2021-01-03 19:49:54 +0400

    Adding log in activity

---
+++ app/src/main/java/ru/maximivanov/ege_helper/LogInActivity.java
@@ -0,0 +1,13 @@
+package ru.maximivanov.ege_helper;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
