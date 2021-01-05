997847fb1da75529cf3be5e362b191665d4d5fbd
m4xxx1m <mi4469228@yandex.ru>
2021-01-05 17:31:56 +0400

    Task 2.7.2, adding signing in and signing up

--- app/src/main/java/ru/maximivanov/ege_helper/LogInActivity.java
+++ app/src/main/java/ru/maximivanov/ege_helper/LogInActivity.java
@@ -1,13 +1,52 @@
 package ru.maximivanov.ege_helper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LogInActivity extends AppCompatActivity {
+
+    // ВРЕМЕННО
+    private final String correctLogin = "admin";
+    private final String correctPassword = "12345678";
+    //
+    private EditText login;
+    private EditText password;
+    private TextView wrong_login_or_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        login = findViewById(R.id.Login);
+        password = findViewById(R.id.Password);
+        wrong_login_or_password = findViewById(R.id.wrong_password);
+    }
+
+    public void clickSignInButton(View view) {
+        String thisLogin, thisPassword;
+        thisLogin = login.getText().toString();
+        thisPassword = password.getText().toString();
+
+        if (thisLogin.equals(correctLogin) && thisPassword.equals(correctPassword)) {
+            Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+        else {
+            wrong_login_or_password.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void clickSignUpButton(View view) {
+        Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
+        String thisLogin, thisPassword;
+        thisLogin = login.getText().toString();
+        thisPassword = password.getText().toString();
+        intent.putExtra("login", thisLogin);
+        intent.putExtra("password", thisPassword);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/ru/maximivanov/ege_helper/SignUpActivity.java
@@ -0,0 +1,21 @@
+package ru.maximivanov.ege_helper;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText login;
+    private EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        login = findViewById(R.id.Login);
+        password = findViewById(R.id.Password);
+        login.setText(getIntent().getStringExtra("login"));
+        password.setText(getIntent().getStringExtra("password"));
+    }
+}
